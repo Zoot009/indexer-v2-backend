5081f8f456b25928b252136234ea442435a94c3c
Zoot009 <zootdigitalseo@gmail.com>
2026-01-05 11:10:04 +0530

    refactor: remove example environment files and update Prisma client initialization

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,23 @@
-import "dotenv/config";
+import { Pool } from 'pg'
 import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '../generated/prisma/client'
+import { PrismaClient } from '../generated/prisma/client';
 
-const connectionString = `${process.env.DATABASE_URL}`
+const globalForPrisma = global as unknown as {
+    prisma: PrismaClient
+}
 
-const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
+const connectionString = process.env.DIRECT_URL || process.env.DATABASE_URL;
 
-export { prisma }
+const pool = new Pool({
+  connectionString,
+})
+
+const adapter = new PrismaPg(pool)
+
+const prisma = globalForPrisma.prisma || new PrismaClient({
+  adapter,
+})
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+export default prisma
